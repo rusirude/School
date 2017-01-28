2f3a2cbc79af22c1cc7449a0b8500c653075dcd8
Rusiru <rusiru091@gmail.com>
2017-01-28 13:55:17 +0530

    correct  repository

--- src/com/leaf/school/entity/SysRoleEntity.java
+++ src/com/leaf/school/entity/SysRoleEntity.java
@@ -1,6 +1,6 @@
 package com.leaf.school.entity;
 
-
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,28 +8,30 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 
 
 @Entity
-@Table(name = "user_management.sys_role")
-public class SysRoleEntity extends BaseEntity{	
+@Table(name = "sys_role")
+public class SysRoleEntity {	
 
-	
-	private static final long serialVersionUID = 5680402398775557307L;
-	private Integer id; 
+	private int id; 
 	private String name;
 	private String description; 
-	
+	private String addedBy; 
+	private Date addedOn;		
+	private String updatedBy;
+	private Date updatedOn;
 	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	@Column(name = "name", nullable = false)
@@ -46,5 +48,38 @@ public class SysRoleEntity extends BaseEntity{
 	public void setDescription(String description) {
 		this.description = description;
 	}
-		
+	@Column(name = "added_by",nullable = false )
+	public String getAddedBy() {
+		return addedBy;
+	}
+	public void setAddedBy(String addedBy) {
+		this.addedBy = addedBy;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "added_on", nullable = false)
+	public Date getAddedOn() {
+		return addedOn;
+	}
+	public void setAddedOn(Date addedOn) {
+		this.addedOn = addedOn;
+	}
+	@Column(name = "updaed_by", nullable = true)
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_on", nullable = true)
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	} 
+	
+	
+	
+	
 }
